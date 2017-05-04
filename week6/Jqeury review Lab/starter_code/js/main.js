3c51b17dd1a25702947024e19ec872055427f062
Stephanie Willhide <stephaniewillhide@gmail.com>
2017-05-04 18:40:49 -0400

    Add week 6, add lab

--- week6/Jqeury review Lab/starter_code/js/main.js
+++ week6/Jqeury review Lab/starter_code/js/main.js
@@ -1,5 +1,20 @@
-$.ready();
 
+// reset input after the user submits
 
-event.preventDefault();
-
+$(document).ready(function(){
+  $('#submit-btn').click(function(){
+      event.preventDefault();
+      var city = $('#city-type').val();
+      $('#city-type').val('');
+      //city = city.toLowerCase().trim();
+      if(city === 'NYC' || city === 'New York' || city === 'New York City'){
+        $('body').attr('class','nyc');
+      }
+      else if(city === 'LA') {
+        $('body').attr('class','la');
+      }
+      else {
+        $('body').attr('class','sydney');
+      }
+  })
+});
