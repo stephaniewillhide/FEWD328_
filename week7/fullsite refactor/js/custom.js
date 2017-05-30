2158d66b50efcdd065f17ed80e99dab47a798f6f
Stephanie Willhide <stephaniewillhide@gmail.com>
2017-05-30 18:27:45 -0400

    Add js

--- week7/fullsite refactor/js/custom.js
+++ week7/fullsite refactor/js/custom.js
@@ -1,1 +1,10 @@
-// Put all your lovely jQuery / Javascript goodies right down here.
+// don't do the js until the page loads
+
+$(document).ready(function(){
+
+// for every element with class accordionButton, on click, run the unnamed function that will toggle show/hide for the next dom element with accordionContent class
+
+    $(".accordionButton").click(function(){
+        $(this).next('.accordionContent').toggle();
+    });
+});
